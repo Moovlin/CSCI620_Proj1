291270dd16733ad121644b69044edff93123f7a7
Ajeeta Khatri <ajeetakhatri@gmail.com>
2019-10-20 22:36:21 -0400

    Add initial structure

--- IMDB_Data_Model/app.py
+++ IMDB_Data_Model/app.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '******' # replace **** with your local mysql password.
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Root@123' # replace **** with your local mysql password.
 app.config['MYSQL_DATABASE_DB'] = 'IMDB_Local'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -13,6 +13,7 @@ mysql.init_app(app)
 def home_page():
     return render_template('home.html')
     #return render_template('Query_1.html')
+
 
 @app.route("/query1")
 def query_1():
@@ -31,6 +32,41 @@ def query_3():
 @app.route("/query3")
 def query_3():
     return render_template('Query_3.html')
+
+
+@app.route("/query4")
+def query_3():
+    return render_template('Query_4.html')
+
+
+@app.route("/query5")
+def query_3():
+    return render_template('Query_5.html')
+
+
+@app.route("/query6")
+def query_3():
+    return render_template('Query_6.html')
+
+
+@app.route("/query7")
+def query_3():
+    return render_template('Query_7.html')
+
+
+@app.route("/query8")
+def query_3():
+    return render_template('Query_8.html')
+
+
+@app.route("/query9")
+def query_3():
+    return render_template('Query_9.html')
+
+
+@app.route("/query10")
+def query_3():
+    return render_template('Query_10.html')
 
 
 @app.route('/result', methods=['POST', 'GET'])
